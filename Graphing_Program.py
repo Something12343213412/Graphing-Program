6e26c050891776551ca5ef594e058e76d05fac30
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-17 11:41:23 -0700

    Added import statements to main and added a window.display.flip into the main while loop

--- Graphing_Program.py
+++ Graphing_Program.py
@@ -1,4 +1,11 @@
 import pygame
+from PositionalVectors import Vector2
+from Shape import Line
 
 size = width, height = 400,500
 screen = pygame.display.set_mode(size)
+
+
+while True:
+
+    window.display.flip()
