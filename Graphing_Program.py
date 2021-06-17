0a7ba5b500b976d65ffc85b9ddf4b892e73cca36
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-17 11:49:47 -0700

    Added lines array that will hold all the graphical lines, will create one for most other shapes. Adde logic to loop through lines and draw them to the screen.

--- Graphing_Program.py
+++ Graphing_Program.py
@@ -2,10 +2,18 @@ import pygame
 from PositionalVectors import Vector2
 from Shape import Line
 
-size = width, height = 400,500
+size = width, height = 400,400
 screen = pygame.display.set_mode(size)
+
+# Creating an array to hold all the different lines in the program
+lines = []
 
 
 while True:
+
+
+
+    for i in range(len(lines)):
+        pygame.draw.line(lines[i])
 
     window.display.flip()
