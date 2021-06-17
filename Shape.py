64a6a7476e07398000aa82dcfb1031ecd58f6158
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-17 11:35:39 -0700

    Changed the init function of Line to include Color and Width

--- Shape.py
+++ Shape.py
@@ -1,9 +1,14 @@
 from PositionalVectors import Vector2
+import pygame
 
-class line:
+class Line:
     
-    def __init__(One : Vector2, Two : Vector2):
+    def __init__(One : Vector2, Two : Vector2, Color : (int,int,int), Width : int):
        self.One = One
        self.Two = Two
+       self.Color = Color
+       self.Width = Width
+
+    
 
 
