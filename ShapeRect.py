231466bfb13cdd2ab8cddc1dc0ac89c35a17774f
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-19 13:03:35 -0700

    Added class Rect and file Shape Rect, used for creating rectangles, added a constructor for the class

---
+++ ShapeRect.py
@@ -0,0 +1,12 @@
+from PositionalVectors import Vector2
+
+# Rectangle class
+class Rect():
+
+    def __init__(self, pos : Vector2, dimensions : Vector2, color):
+        self.x = pos.x
+        self.y = pos.y
+
+    
+
+
