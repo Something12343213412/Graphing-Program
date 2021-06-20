5c91d84b7ddd31a1e19e3a658357416936744774
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-20 09:49:34 -0700

    Changed setBorder to be a more complicated __init__ function so there wouldn't be any extra function calls

--- ShapeRectBorder.py
+++ ShapeRectBorder.py
@@ -3,8 +3,17 @@ from ShapeRect import Rect
 
 class RectBorder(Rect):
      
-    def setBorder():
-        pass
+    def __init__(self, pos : Vector2, dimensions : Vector2, color : (int,int,int), borderWidth : int, borderColor : (int,int,int)):
+        self.pos = pos
+        self.dimensions = dimensions
+        self.color = color
+        
+        # Creating an object that the pygame renderer has to do no conversions for
+        self.pygameRect = pygame.Rect(pos.x,pos.y,dimensions.x,dimensions.y)
+        
+        # Creating the border of the object and color of the border
+        self.borderWidth = borderWidth
+        self.borderColor = borderColor
      
 
 
