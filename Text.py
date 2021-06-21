f39a8afdf38332e2ed32566bc252ff908fe1595d
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-21 16:15:08 -0700

    Added testcase and displaying the text

--- Text.py
+++ Text.py
@@ -6,5 +6,9 @@ pygame.font.init()
 
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
-# Mapping the font to a variable
-textsurface = myfont.render('Some Text', False, (0, 0, 0))
+
+def test():
+    # Mapping the font to a variable
+    textsurface = myfont.render('Some Text', False, (0, 0, 0))
+
+    screen.blit(textsurface,(0,0))
