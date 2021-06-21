b41e405a90407efe86d7d9c9016fdd8a056808ae
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-21 16:46:17 -0700

    Changed the __init__ function to fit the class, and added getTextInformation

--- Button.py
+++ Button.py
@@ -1,8 +1,35 @@
 
 from ShapeRectBorder import RectBorder
 from PositionalVectors import Vector2
+import Text
 import pygame
 
 class Button(RectBorder):
+    
+    # constructor
+    def __init__(self, pos : Vector2, dimensions : Vector2, color : (int,int,int), borderWidth : int, borderColor : (int,int,int), textColor : (int,int,int), textPosition : Vector2):
+        self.pos = pos
+        self.dimensions = dimensions
+        self.color = color
+        
+        # Creating an object that the pygame renderer has to do no conversions for
+        self.pygameRect = pygame.Rect(pos.x,pos.y,dimensions.x,dimensions.y)
+        
+        # Creating the border of the object and color of the border
+        self.borderWidth = borderWidth
+        self.borderColor = borderColor
 
+        # Creating the border rectangle that is going nto be behind the first
+        self.borderRectangles = pygame.Rect(pos.x - borderWidth, pos.y - borderWidth, dimensions.x + borderWidth * 2, dimensions.y + borderWidth * 2)
+
+        # Information about the text
+        self.textPos = textPosition
+        self.textColor = textColor
+
+        # Pressing the button information
+        self.isPressed = false
+
+    def getTextInformation():
+
+        
     
