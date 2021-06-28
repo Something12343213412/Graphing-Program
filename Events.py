416fef846f55c00218fb1fcde709a2ca1db44884
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-28 10:46:50 -0700

    Added a rectangle to be the background of the main menu

--- Events.py
+++ Events.py
@@ -1,6 +1,7 @@
 import pygame
 import Shapes
 import Button
+import ShapeRect
 from PositionalVectors import Vector2
 
 
@@ -11,9 +12,10 @@ def closeProgram():
     pygame.quit()
 
 
-
 def startingEvent():
+    Shapes.rectangles.append(ShapeRect.Rect(Vector2(0,0), Vector2(1280,720), (71, 121, 201)))
     Shapes.buttons.append(Button.Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(40,45),1, 30))
+    
     
 def linkToEvent(event : int):
     if event == 0:
