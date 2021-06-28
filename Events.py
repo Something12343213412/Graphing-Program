ec714add0d47a63d21f12bdae2c5db292b8804a2
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-28 10:20:48 -0700

    Added startingEvent

--- Events.py
+++ Events.py
@@ -8,6 +8,8 @@ def testEvent():
 def closeProgram():
     pygame.quit()
 
+def startingEvent():
+    buttons.append(Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(40,45),1, 30))
     
 def LinkToEvent(event : int):
     if event == 0:
