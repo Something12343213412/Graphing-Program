02c1fc9f22e2d2f71c057df98b67513014b7a4a7
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-28 10:29:58 -0700

    Fixed logic and added starting event to the linked event list

--- Events.py
+++ Events.py
@@ -1,5 +1,7 @@
 import pygame
 import Shapes
+import Button
+from PositionalVectors import Vector2
 
 
 def testEvent():
@@ -9,7 +11,7 @@ def closeProgram():
     pygame.quit()
 
 def startingEvent():
-    buttons.append(Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(40,45),1, 30))
+    Shapes.buttons.append(Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(40,45),1, 30))
     
 def LinkToEvent(event : int):
     if event == 0:
@@ -17,3 +19,6 @@ def LinkToEvent(event : int):
 
     if event == 1:
         closeProgram()
+
+    if event == 2:
+        startingEvent()
--- Graphing_Program.py
+++ Graphing_Program.py
@@ -8,35 +8,14 @@ from Button import Button
 import Rendering
 import Text
 import Shapes
+import Events
 
 # Creating the clock
 clock = pygame.time.Clock()
 
 
-# Creating an array to hold all the different lines in the program
-lines = []
-
-# Creating a new line
-
-# Example of how to add a line : lines.append(Line(Vector2(1,1), Vector2(200,200),[200,50,50],10))
-
-# Creating an array to hold all the different rectangles in the program
-rectangles = []
-
-# creating an array to hold all the different rectangles with borders in the program
-borderRectangles = []
-
-# Creating a new rectangle
-rectangles.append(Rect(Vector2(0,0),Vector2(400,400),[50,50,200]))
-
-# Creating a new rectangle with a border
-#borderRectangles.append(RectBorder(Vector2(100,100),Vector2(20,20),[75,75,150],5,[0,0,0]))
-
-# Creating an array to hold all the buttons
-buttons = []
-
-# adding objects to the buttons list
-buttons.append(Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(40,45),1, 30))
+# Calling the starting event
+Events.LinkToEvent(2)
 
 while True:
 
