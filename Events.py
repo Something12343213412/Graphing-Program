d89713279f7fb938c8ef3f30560ff5c613609bbf
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-28 11:01:26 -0700

    Changing colors of buttons to make them look nicer

--- Events.py
+++ Events.py
@@ -17,10 +17,10 @@ def startingEvent():
     Shapes.rectangles.append(ShapeRect.Rect(Vector2(0,0), Vector2(1280,720), (71, 121, 201)))
 
     # Close Program Button
-    Shapes.buttons.append(Button.Button(Vector2(40,40),Vector2(200,60),[75,75,150],5,[0,0,0], "Close Program",[0,255,0], Vector2(0,5),1, 30))
+    Shapes.buttons.append(Button.Button(Vector2(40,40),Vector2(200,60),[175,75,75],5,[0,0,0], "Close Program",[0,255,0], Vector2(5,5),1, 30))
 
     # Setting Button
-    Shapes.buttons.append(Button.Button(Vector2(1000,40),Vector2(150,60),[75,75,150],5,[0,0,0], "Settings",[0,255,0], Vector2(15,5),1, 30))
+    Shapes.buttons.append(Button.Button(Vector2(1000,40),Vector2(150,60),[75,75,75],5,[0,0,0], "Settings",[200,200,200], Vector2(15,5),1, 30))
 
     
     
