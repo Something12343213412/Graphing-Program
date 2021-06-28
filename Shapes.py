89ff3a958aca4b7b48c00dff2e9c6157fad78511
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-28 10:16:40 -0700

    Added the different shape arrays to shapes

--- Shapes.py
+++ Shapes.py
@@ -1,1 +1,12 @@
 
+# Creating an array to hold all the different lines in the program
+lines = []
+
+# Creating an array to hold all the different rectangles in the program
+rectangles = []
+
+# creating an array to hold all the different rectangles with borders in the program
+borderRectangles = []
+
+# Creating an array to hold all the buttons
+buttons = []
