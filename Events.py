109d85666ad37fb27c12fbf6c367ab6dd8d1a18a
Kevin Sandberg <Kevbo2007@icloud.com>
2021-07-01 10:09:59 -0700

    Added the settings and Graphing menu

--- Events.py
+++ Events.py
@@ -4,6 +4,7 @@ import Button
 import Text
 import ShapeRect
 from PositionalVectors import Vector2
+import ShapeRectBorder
 
 # Creates the basic home screen, called at the start of the program
 def startingEvent():
@@ -19,7 +20,7 @@ def startingEvent():
     Shapes.buttons.append(Button.Button(Vector2(1000,40),Vector2(150,60),[75,75,75],5,[0,0,0], "Settings",[0,0,0], Vector2(15,5),5, 30))
 
     # Start Button
-    Shapes.buttons.append(Button.Button(Vector2(390,330),Vector2(500,120),[28, 165, 179],5,[0,0,0], "Start Program",[0,0,0], Vector2(5,5),1, 70))
+    Shapes.buttons.append(Button.Button(Vector2(390,330),Vector2(500,120),[28, 165, 179],5,[0,0,0], "Start Program",[0,0,0], Vector2(5,5),6, 70))
 
     # Credits Button
     Shapes.buttons.append(Button.Button(Vector2(580,530),Vector2(120,60),[125,125,125],5,[0,0,0], "Credits",[0,0,0], Vector2(5,5),3, 30))
@@ -29,6 +30,29 @@ def testEvent():
 
 def closeProgram():
     pygame.quit()
+
+def graphingMenu():
+    Shapes.runLoop = False
+
+    Shapes.clearAll()
+    # Background
+    Shapes.rectangles.append(ShapeRect.Rect(Vector2(0,0), Vector2(1280,720), (180, 180, 180)))
+
+    # Close Program Button
+    Shapes.buttons.append(Button.Button(Vector2(40,40),Vector2(200,60),[150,75,75],5,[0,0,0], "Close Program",[0,0,0], Vector2(5,5),1, 30))
+
+    # Back Button
+    Shapes.buttons.append(Button.Button(Vector2(40,600),Vector2(75,60),[150,150,150],5,[0,0,0], "Back",[0,0,0], Vector2(5,5),4, 30))
+
+    # Setting Button
+    Shapes.buttons.append(Button.Button(Vector2(1000,40),Vector2(150,60),[75,75,75],5,[0,0,0], "Settings",[0,0,0], Vector2(15,5),5, 30))
+
+    # Graphing Rectangle
+    Shapes.borderRectangles.append(ShapeRectBorder.RectBorder(Vector2(290,140),Vector2(800,500),[190,190,190],10,[0,0,0]))
+
+    # Inputs for graphing
+
+
 
 def settingsMenu():
     Shapes.runLoop = False
@@ -84,3 +108,6 @@ def linkToEvent(event : int):
 
     if event == 5:
         settingsMenu()
+
+    if event == 6:
+        graphingMenu()
