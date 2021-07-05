05033f2177baeb159c8f46c80ce9be380bb2d0d9
Kevin Sandberg <Kevbo2007@icloud.com>
2021-07-05 10:28:17 -0700

    Added functions init, changeState, and stepBackAState

--- State.py
+++ State.py
@@ -4,8 +4,18 @@ creditsMenu = 3
 graphingMenu = 4
 
 
-class State(object):
+class State():
     
-    
+    def __init__(self):
+        self.currentState = mainMenu
+        self.previousStates = [mainMenu]
+
+    def changeState(self, newState):
+        self.previousState.append(self.currentState)
+        self.currentState = newState
+
+    def stepBackAState(self):
+        self.currentState = self.previousStates[-1]
+        self.previousStates.pop(-1)
 
 
